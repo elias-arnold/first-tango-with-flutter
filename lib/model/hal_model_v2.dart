48b84d63580f9bbb799e94c03c0ea17dca7330fc
Elias <e.arnold@soul-surf.com>
2022-09-02 16:23:06 +0200

    Update hal_model_v2.dart

--- lib/model/hal_model_v2.dart
+++ lib/model/hal_model_v2.dart
@@ -1,8 +1,4 @@
-abstract class FromJson {
-  FromJson.fromJson();
-}
-
-class HalRequestResult implements FromJson {
+class HalRequestResult {
   final List<dynamic> elements;
   final Page page;
   final Map<String, HalLink> links;
@@ -17,14 +13,16 @@ class HalRequestResult implements FromJson {
       mylinks.putIfAbsent(element.key, () => HalLink.fromJson(element.value));
     });
     return HalRequestResult(
-        elements:
-            (json['_embedded'] as Map<String, dynamic>).entries.first.value as List<dynamic>,
+        elements: (json['_embedded'] as Map<String, dynamic>)
+            .entries
+            .first
+            .value as List<dynamic>,
         page: Page.fromJson(json['page']),
         links: mylinks);
   }
 }
 
-class HalLink implements FromJson {
+class HalLink {
   final String href;
 
   const HalLink({required this.href});
@@ -34,7 +32,7 @@ class HalLink implements FromJson {
   }
 }
 
-class Page implements FromJson {
+class Page {
   final int size;
   final int totalElements;
   final int totalPages;
@@ -55,3 +53,53 @@ class Page implements FromJson {
     );
   }
 }
+
+class HalRequestObject {
+  final Map<String, HalLink> links;
+
+  HalRequestObject(this.links);
+
+  factory HalRequestObject.fromJson(Map<String, dynamic> json) {
+    Map<String, dynamic> newLinks = json['_links'];
+    Map<String, HalLink> mylinks = {};
+    newLinks.entries.forEach((element) {
+      mylinks.putIfAbsent(element.key, () => HalLink.fromJson(element.value));
+    });
+
+    return HalRequestObject(mylinks);
+  }
+}
+
+class Client extends HalRequestObject {
+  final String code;
+  final String configFileName;
+  final String configJson;
+  final String country;
+  final String name;
+  @override
+  final Map<String, HalLink> links;
+
+  Client(this.links,
+      {required this.code,
+      required this.configFileName,
+      required this.configJson,
+      required this.country,
+      required this.name})
+      : super(links);
+
+  factory Client.fromJson(Map<String, dynamic> json) {
+    return Client(HalRequestObject.fromJson(json).links,
+        code: json['code'],
+        configFileName: json['configFileName'],
+        configJson: json['configJson'],
+        country: json['country'],
+        name: json['name']);
+  }
+}
+
+// class Clients extends HalRequestResult {
+//
+//   factory Clients.fromJson(Map<String, dynamic> json): super.fromJson(json){
+//
+//   };
+// }
